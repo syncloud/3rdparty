e693342bfaec11163dbc4bd0bc4249827e9d9481
vsapronov <vladimir.sapronov@gmail.com>
2015-06-12 01:23:21 -0400

    Added guestAuth for TeamCity

--- openldap/build-chroot.sh
+++ openldap/build-chroot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ROOT_FS_FILENAME="rootfs.tar.gz"
-wget -O ${ROOT_FS_FILENAME} http://build.syncloud.org:8111/repository/download/image_armv7l_rootfs/lastSuccessful/${ROOT_FS_FILENAME}
+wget -O ${ROOT_FS_FILENAME} http://build.syncloud.org:8111/guestAuth/repository/download/image_armv7l_rootfs/lastSuccessful/${ROOT_FS_FILENAME}
 
 tar xzf rootfs.tar.gz
 cp -r ./* rootfs/root
