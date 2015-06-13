07e81c80f7003d51dd0d0f0898238a72aa13caf9
Boris Rybalkin <ribalkin@gmail.com>
2015-06-13 12:24:29 +0100

    fixing ruby build

--- ruby/build.sh
+++ ruby/build.sh
@@ -17,20 +17,5 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 
 curl -sSL https://get.rvm.io | bash -s stable --ruby=${VERSION}
 
-apt-get -y install build-essential flex bison libreadline-dev zlib1g-dev libpcre3-dev
-
-rm -rf build
-mkdir -p build
-cd build
-
-wget http://nginx.org/download/${NAME}-${VERSION}.tar.gz
-tar xzf ${NAME}-${VERSION}.tar.gz
-cd ${NAME}-${VERSION}
-./configure --prefix=${PREFIX}
-make -j2
-rm -rf ${PREFIX}
-make install
-cd ../..
-
 rm -rf ${NAME}.tar.gz
 tar cpzf ${NAME}.tar.gz -C  ${HOME}/.rvm/rubies ${NAME}-${VERSION}
