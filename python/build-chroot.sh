178fe9a1a5ad108feaa105eb037fbeb8d0df58a1
Boris Rybalkin <ribalkin@gmail.com>
2015-06-13 12:52:15 +0100

    less verbose chroot progress

--- python/build-chroot.sh
+++ python/build-chroot.sh
@@ -4,7 +4,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
 ROOT_FS_FILENAME="rootfs.tar.gz"
-wget -O ${ROOT_FS_FILENAME} http://build.syncloud.org:8111/guestAuth/repository/download/image_armv7l_rootfs/lastSuccessful/${ROOT_FS_FILENAME}
+wget -O ${ROOT_FS_FILENAME} http://build.syncloud.org:8111/guestAuth/repository/download/image_armv7l_rootfs/lastSuccessful/${ROOT_FS_FILENAME} --progress dot:giga
 
 VERSION=2.7.10
 SOURCES_URL=https://www.python.org/ftp/python/${VERSION}/Python-${VERSION}.tgz
