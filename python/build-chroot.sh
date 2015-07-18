414c0e13210ad044fa31c763504c6c490a9ab845
Boris Rybalkin <ribalkin@gmail.com>
2015-07-18 09:07:32 +0100

    fixing chroot

--- python/build-chroot.sh
+++ python/build-chroot.sh
@@ -3,6 +3,12 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export DEBCONF_FRONTEND=noninteractive
+export DEBIAN_FRONTEND=noninteractive
+export TMPDIR=/tmp
+export TMP=/tmp
+
 ROOT_FS_FILENAME="rootfs.tar.gz"
 wget -O ${ROOT_FS_FILENAME} http://build.syncloud.org:8111/guestAuth/repository/download/image_armv7l_rootfs/lastSuccessful/${ROOT_FS_FILENAME} --progress dot:giga
 
