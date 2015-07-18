89149e71cfa073b29517e8aeee0cef3be3c1dc84
Boris Rybalkin <ribalkin@gmail.com>
2015-07-18 20:02:27 +0100

    fixing wrapper

--- python/build.sh
+++ python/build.sh
@@ -11,7 +11,7 @@ export TMP=/tmp
 NAME=python
 PREFIX=${DIR}/build/${NAME}
 
-apt-get -y install build-essential flex bison libreadline-dev zlib1g-dev libpcre3-dev libssl-dev
+apt-get -y install build-essential flex bison libreadline-dev zlib1g-dev libpcre3-dev libssl-dev libbz2-dev
 
 rm -rf build
 mkdir build
@@ -21,5 +21,5 @@ unzip master.zip
 export PYTHON_CONFIGURE_OPTS="--enable-shared --enable-unicode=ucs4"
 ./pyenv-master/plugins/python-build/bin/python-build ${VERSION} ${PREFIX}
 mv ${PREFIX}/bin/python ${PREFIX}/bin/python.bin
-cp python ${PREFIX}/bin/
+cp ${DIR}/python ${PREFIX}/bin/
 tar cpzf python.tar.gz ${NAME}
