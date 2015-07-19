61be71bd57dde1d29ce8690111f1a0b0ee3ecb2a
Boris Rybalkin <ribalkin@gmail.com>
2015-07-19 13:18:54 +0100

    fixing ruby key

--- ruby/build.sh
+++ ruby/build.sh
@@ -13,7 +13,8 @@ PREFIX=${ROOT}/${NAME}
 
 echo "building ${NAME}"
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+#gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+command curl -sSL https://rvm.io/mpapis.asc | sudo gpg --import -
 
 curl -sSL https://get.rvm.io | bash -s stable --ruby=${VERSION}
 
