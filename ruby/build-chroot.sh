b4b400a8ee647f9354809fb3121c87faca8d53bf
Boris Rybalkin <ribalkin@gmail.com>
2015-07-19 13:12:19 +0100

    fixing rootfs

--- ruby/build-chroot.sh
+++ ruby/build-chroot.sh
@@ -3,9 +3,14 @@
 ROOT_FS_FILENAME="rootfs.tar.gz"
 wget -O ${ROOT_FS_FILENAME} http://build.syncloud.org:8111/guestAuth/repository/download/image_armv7l_rootfs/lastSuccessful/${ROOT_FS_FILENAME} --progress dot:giga
 
-tar xzf rootfs.tar.gz
-cp -r ./* rootfs/root
 
 mount | grep rootfs | awk '{print "umounting "$1; system("umount "$3)}'
+
+rm -rf rootfs
+mkdir rootfs
+
+tar xzf rootfs.tar.gz -C rootfs
+cp -r ./* rootfs/root
+
 chroot rootfs /bin/bash -c "mount -t proc proc /proc"
 chroot rootfs root/build.sh
