1dea6c3d38eb352bb7623c010809dfc2c91e33b1
Boris Rybalkin <ribalkin@gmail.com>
2015-07-20 23:43:46 +0100

    trying movable mode

--- ruby/build.sh
+++ ruby/build.sh
@@ -13,16 +13,20 @@ PREFIX=${ROOT}/${NAME}
 
 echo "building ${NAME}"
 
-#gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 command curl -sSL https://rvm.io/mpapis.asc | sudo gpg --import -
 
-curl -sSL https://get.rvm.io | bash -s stable --ruby=${VERSION}
+#useradd -p ruby ruby
 
+rm -rf /tmp/rvm
+
+curl -sSL https://get.rvm.io | bash -s stable --path /tmp/rvm
+source /tmp/rvm/scripts/rvm
+rvm install ${VERSION} --movable
 rm -rf ${DIR}/${NAME}.tar.gz
 
 rm -rd ${DIR}/build
 mkdir ${DIR}/build
 
-cp -r /usr/local/rvm/rubies/${NAME}-${VERSION} ${DIR}/build/ruby
+cp -r /tmp/rvm/rubies/${NAME}-${VERSION} ${DIR}/build/ruby
 
 tar cpzf ${DIR}/${NAME}.tar.gz -C ${DIR}/build ruby
