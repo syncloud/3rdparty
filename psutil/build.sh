5f05286d667bcfb33ded9da9c00e251a1870a508
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2015-07-24 10:04:20 -0400

    Less verbose output

--- psutil/build.sh
+++ psutil/build.sh
@@ -12,7 +12,7 @@ FULL_NAME="${NAME}-${VERSION}"
 echo "building ${NAME}"
 
 apt-get -y install python
-wget -O get-pip.py https://bootstrap.pypa.io/get-pip.py
+wget -O get-pip.py https://bootstrap.pypa.io/get-pip.py --progress dot:giga
 python get-pip.py
 rm get-pip.py
 pip install wheel
@@ -24,8 +24,8 @@ mkdir build
 
 cd ${DIR}/build
 mkdir ${NAME}
-wget -O ${FULL_NAME}.tar.gz https://pypi.python.org/packages/source/p/${NAME}/${FULL_NAME}.tar.gz
-tar zxvf ${FULL_NAME}.tar.gz -C ${NAME} --strip-components=1
+wget -O ${FULL_NAME}.tar.gz https://pypi.python.org/packages/source/p/${NAME}/${FULL_NAME}.tar.gz --progress dot:giga
+tar zxf ${FULL_NAME}.tar.gz -C ${NAME} --strip-components=1
 rm -rf ${FULL_NAME}.tar.gz
 
 cd ${NAME}
