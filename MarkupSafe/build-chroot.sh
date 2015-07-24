2723314bdb24d447151ec020606c34b02473e017
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2015-07-24 00:50:25 -0400

    Fixing build for arm

--- MarkupSafe/build-chroot.sh
+++ MarkupSafe/build-chroot.sh
@@ -3,6 +3,8 @@
 ROOT_FS_FILENAME="rootfs.tar.gz"
 wget -O ${ROOT_FS_FILENAME} http://build.syncloud.org:8111/guestAuth/repository/download/image_armv7l_rootfs/lastSuccessful/${ROOT_FS_FILENAME} --progress dot:giga
 
-tar xzf rootfs.tar.gz
-cp -r ./* rootfs/root
+mkdir rootfs
+
+tar xzf rootfs.tar.gz -C rootfs
+cp -r build.sh rootfs/root
 chroot rootfs root/build.sh
