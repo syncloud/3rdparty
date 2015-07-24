9cdc47422ff6c4f6ee9760c3f30d1196a17c8b4b
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2015-07-24 10:04:33 -0400

    Less verbose output + fixing arm build

--- MarkupSafe/build.sh
+++ MarkupSafe/build.sh
@@ -12,20 +12,20 @@ FULL_NAME="${NAME}-${VERSION}"
 echo "building ${NAME}"
 
 apt-get -y install python
-wget -O get-pip.py https://bootstrap.pypa.io/get-pip.py
+wget -O get-pip.py https://bootstrap.pypa.io/get-pip.py --progress dot:giga
 python get-pip.py
 rm get-pip.py
 pip install wheel
 
-apt-get install -y python-dev
+apt-get install -y build-essential python-dev
 
 rm -rf build
 mkdir build
 
 cd ${DIR}/build
 mkdir ${NAME}
-wget -O ${FULL_NAME}.tar.gz https://pypi.python.org/packages/source/M/${NAME}/${FULL_NAME}.tar.gz
-tar zxvf ${FULL_NAME}.tar.gz -C ${NAME} --strip-components=1
+wget -O ${FULL_NAME}.tar.gz https://pypi.python.org/packages/source/M/${NAME}/${FULL_NAME}.tar.gz --progress dot:giga
+tar zxf ${FULL_NAME}.tar.gz -C ${NAME} --strip-components=1
 rm -rf ${FULL_NAME}.tar.gz
 
 cd ${NAME}
