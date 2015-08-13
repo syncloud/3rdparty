3296e1d2a67dc9e72b9602ee1303cfe670428218
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2015-08-13 08:26:59 -0400

    Adding architecture to package name

--- nginx/build.sh
+++ nginx/build.sh
@@ -4,6 +4,8 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
+ARCH=$(dpkg-architecture -qDEB_HOST_GNU_CPU)
+
 export TMPDIR=/tmp
 export TMP=/tmp
 NAME=nginx
@@ -28,5 +30,5 @@ rm -rf ${PREFIX}
 make install
 cd ../..
 
-rm -rf ${NAME}.tar.gz
-tar cpzf ${NAME}.tar.gz -C ${ROOT} ${NAME}
+rm -rf ${NAME}-${ARCH}.tar.gz
+tar cpzf ${NAME}-${ARCH}.tar.gz -C ${ROOT} ${NAME}
