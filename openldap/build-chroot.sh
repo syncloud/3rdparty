d6799f2b1de137795f7113b798da10cd2d24663c
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2015-08-13 23:21:56 -0400

    Fixing rootfs unpacking

--- openldap/build-chroot.sh
+++ openldap/build-chroot.sh
@@ -3,6 +3,8 @@
 ROOT_FS_FILENAME="rootfs.tar.gz"
 wget -O ${ROOT_FS_FILENAME} http://build.syncloud.org:8111/guestAuth/repository/download/image_armv7l_rootfs/lastSuccessful/${ROOT_FS_FILENAME} --progress dot:giga
 
-tar xzf rootfs.tar.gz
+rm -rf rootfs
+mkdir rootfs
+tar xzf ${ROOT_FS_FILENAME} -C rootfs
 cp -r ./* rootfs/root
 chroot rootfs root/build.sh
