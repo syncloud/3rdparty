e218831a26ad12c07d80a49a95a99992ea05c753
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2015-08-14 22:18:24 -0400

    Fixing rootfs unpacking

--- ruby/build-chroot.sh
+++ ruby/build-chroot.sh
@@ -3,26 +3,18 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
-ROOTFS=/tmp/ruby/rootfs
+ROOTFS=rootfs
 
-if [ ! -d ${DIR}/3rdparty ]; then
-    mkdir ${DIR}/3rdparty
-fi
-
-if [ ! -f ${DIR}/3rdparty/rootfs.tar.gz ]; then
-    wget -O ${DIR}/3rdparty/rootfs.tar.gz http://build.syncloud.org:8111/guestAuth/repository/download/image_armv7l_rootfs/lastSuccessful/rootfs.tar.gz --progress dot:giga
-else
-    echo "skipping rootfs.tar.gz"
-fi
+ROOT_FS_FILENAME="rootfs.tar.gz"
+wget -O ${ROOT_FS_FILENAME} http://build.syncloud.org:8111/guestAuth/repository/download/image_armv7l_rootfs/lastSuccessful/${ROOT_FS_FILENAME} --progress dot:giga
 
 mount | grep ${ROOTFS} | awk '{print "umounting "$1; system("umount "$3)}'
 
 rm -rf ${ROOTFS}
-mkdir -p ${ROOTFS}
+mkdir ${ROOTFS}
 
-tar xzf ${DIR}/3rdparty/rootfs.tar.gz -C ${ROOTFS}
+tar xzf ${ROOT_FS_FILENAME} -C ${ROOTFS}
 cp -r ./* ${ROOTFS}/root
 
 chroot ${ROOTFS} /bin/bash -c "mount -t proc proc /proc"
 chroot ${ROOTFS} root/build.sh
-cp ${ROOTFS}/root/ruby.tar.gz .
