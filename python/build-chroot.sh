ed7eb5f6c2ff127672ea62610becb14600d7f9ed
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2015-08-15 13:18:14 -0400

    Fixing arm build: No copy everything inside of rootfs; architecture is armv7l (not just arm)...

--- python/build-chroot.sh
+++ python/build-chroot.sh
@@ -15,5 +15,7 @@ wget -O ${ROOT_FS_FILENAME} http://build.syncloud.org:8111/guestAuth/repository/
 rm -rf rootfs
 mkdir rootfs
 tar xzf ${ROOT_FS_FILENAME} -C rootfs
-cp -r ./* rootfs/root
-chroot rootfs root/build.sh
+
+cp build.sh rootfs/root
+cp python rootfs/root
+chroot rootfs root/build.sh armv7l
