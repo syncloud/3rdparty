ada2544c46b3d4464c7d2cfbf483df18088c31e0
Boris Rybalkin <ribalkin@gmail.com>
2015-09-06 00:04:48 +0100

    1. forcing HOME to /root to fix ruby build 2. extracting chroot-build to share with others

---
+++ tools/build-chroot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+HOME=/root
+ROOTFS=/tmp/rootfs
+ROOTFS_FILENAME="rootfs.tar.gz"
+ROOTFS_FILE="/tmp/${ROOTFS_FILENAME}"
+
+if [ ! -f ${ROOTFS_FILE} ]; then
+   wget -O ${ROOTFS_FILE} http://build.syncloud.org:8111/guestAuth/repository/download/image_armv7l_rootfs/lastSuccessful/${ROOTFS_FILENAME} --progress dot:giga
+else
+    echo "${ROOTFS_FILE} is here"
+fi
+
+rm -rf ${ROOTFS}
+mkdir ${ROOTFS}
+
+tar xzf ${ROOTFS_FILE} -C ${ROOTFS}
+
+cp build.sh ${ROOTFS}/root
+chroot ${ROOTFS} root/build.sh armv7l
