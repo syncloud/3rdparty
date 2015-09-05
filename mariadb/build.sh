9e79a288ea15ceb5728159df02b03ec183cd37a3
Boris Rybalkin <ribalkin@gmail.com>
2015-09-06 00:23:43 +0100

    adding mariadb as looks like owncloud's file scan is not working properly with postgresql

---
+++ mariadb/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd ${DIR}
+
+if [[ -z "$1" ]]; then
+    echo "usage $0 architecture"
+    exit 1
+fi
+
+ARCH=$1
+
+export TMPDIR=/tmp
+export TMP=/tmp
+NAME=mariadb
+VERSION=10.0.21
+BUILD=${DIR}/build
+PREFIX=${BUILD}/${NAME}
+
+apt-get -y install build-essential cmake
+
+rm -rf ${BUILD}
+mkdir ${BUILD}
+cd ${BUILD}
+
+wget https://downloads.mariadb.org/f/${NAME}-${VERSION}/source/${NAME}-${VERSION}.tar.gz/from/http%3A/mirrors.coreix.net/mariadb?serve \
+    --progress dot:giga -O ${NAME}-${VERSION}.tar.gz
+tar xzf ${NAME}-${VERSION}.tar.gz
+cd ${NAME}-${VERSION}
+
+
+./configure --prefix ${PREFIX}
+make
+make install
+
+cd ${BUILD}
+
+rm -rf ${NAME}-${ARCH}.tar.gz
+tar czf ${NAME}-${ARCH}.tar.gz -C ${ROOT} ${NAME}
