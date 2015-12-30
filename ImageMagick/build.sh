fba80f360a29cf21568aeab82328a64d754e18d8
Boris Rybalkin <ribalkin@gmail.com>
2015-12-30 22:43:53 +0000

    fix dependences

--- ImageMagick/build.sh
+++ ImageMagick/build.sh
@@ -21,7 +21,7 @@ echo "building ${NAME}"
 
 apt-get -y install build-essential \
     libxml2-dev autoconf libjpeg-dev libpng12-dev libfreetype6-dev \
-    libzip2 libzip-dev zlib1g-dev libcurl4-gnutls-dev dpkg-dev \
+    libzip-dev zlib1g-dev libcurl4-gnutls-dev dpkg-dev \
     libpq-dev libreadline-dev libldap2-dev libsasl2-dev libssl-dev libldb-dev \
     p7zip libtool
 
