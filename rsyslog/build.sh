12f09d440241b8ecdfb173f2d2197045c0afc4a1
Boris Rybalkin <ribalkin@gmail.com>
2016-02-28 10:40:20 +0000

    adding rsyslog

---
+++ rsyslog/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd ${DIR}
+
+if [[ -z "$1" ]]; then
+    echo "usage $0 architecture"
+    exit 1
+fi
+
+ARCH=$1
+
+export TMPDIR=/tmp
+export TMP=/tmp
+NAME=rsyslog
+VERSION=8.16.0
+BUILD=${DIR}/build
+PREFIX=${BUILD}/${NAME}
+
+apt-get -y install build-essential cmake libncurses5-dev
+
+rm -rf ${BUILD}
+mkdir ${BUILD}
+cd ${BUILD}
+
+wget http://www.rsyslog.com/files/download/rsyslog/{NAME}-${VERSION}.tar.gz \
+    --progress dot:giga
+tar xzf ${NAME}-${VERSION}.tar.gz
+cd ${NAME}-${VERSION}
+
+./configure --prefix=${PREFIX}
+make
+make install
+
+rm -rf ${BUILD}/${NAME}-${ARCH}.tar.gz
+tar czf ${DIR}/${NAME}-${ARCH}.tar.gz -C ${BUILD} ${NAME}
