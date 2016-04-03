91931f499c7319e8e2b72fc028b8e8a89c8f591e
Boris Rybalkin <ribalkin@gmail.com>
2016-04-03 19:26:47 +0100

    adding srtp library for webrtc

--- asterisk/build.sh
+++ asterisk/build.sh
@@ -19,7 +19,8 @@ BASE_DIR=/opt/app/talk
 PREFIX=${BASE_DIR}/${NAME}
 
 apt-get -y install build-essential cmake libncurses5-dev libldap2-dev libsasl2-dev libssl-dev libldb-dev \
-    uuid-dev libjansson-dev libxslt1-dev liburiparser1 libxml2 libsqlite3-0 libicu-dev libcurl3-gnutls
+    uuid-dev libjansson-dev libxslt1-dev liburiparser1 libxml2 libsqlite3-0 libicu-dev libcurl3-gnutls \
+    libsrtp0-dev
 
 rm -rf ${PREFIX}
 mkdir -p ${PREFIX}
@@ -47,6 +48,7 @@ cp --remove-destination /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libxm
 cp --remove-destination /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libsqlite3.so* ${PREFIX}/lib
 cp --remove-destination /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libicu*.so* ${PREFIX}/lib
 cp --remove-destination /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libcurl-gnutls.so* ${PREFIX}/lib
+cp --remove-destination /usr/lib/libsrtp.so* ${PREFIX}/lib
 
 rm -rf ${BUILD}/${NAME}-${ARCH}.tar.gz
 tar czf ${DIR}/${NAME}-${ARCH}.tar.gz -C ${BASE_DIR} ${NAME}
