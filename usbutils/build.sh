998651aca6e3cf75d71979327e18632ff48aca78
Boris Rybalkin <ribalkin@gmail.com>
2016-04-14 07:16:04 +0100

    local cache dir

--- usbutils/build.sh
+++ usbutils/build.sh
@@ -36,7 +36,7 @@ mkdir -p ${BUILD_DIR}
 
 cp -r ${DIR}/bin ${BUILD_DIR}
 
-coin --to ${BUILD_DIR} deb http://http.us.debian.org/debian/pool/main/u/usbutils/usbutils_007-2_${ARCH_DEB}.deb --subfolder usbutils
+coin --cache_folder ${DIR}/cache.dir --to ${BUILD_DIR} deb http://http.us.debian.org/debian/pool/main/u/usbutils/usbutils_007-2_${ARCH_DEB}.deb --subfolder usbutils
 coin --to ${BUILD_DIR} deb http://http.us.debian.org/debian/pool/main/libu/libusb-1.0/libusb-1.0-0_1.0.19-1_${ARCH_DEB}.deb --subfolder libusb
 
 echo "zipping"
