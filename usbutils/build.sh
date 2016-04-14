f44bf2f2f3df438094bed20923a3fc7f3bc8090c
Boris Rybalkin <ribalkin@gmail.com>
2016-04-14 07:57:19 +0100

    more cache dir

--- usbutils/build.sh
+++ usbutils/build.sh
@@ -37,7 +37,7 @@ mkdir -p ${BUILD_DIR}
 cp -r ${DIR}/bin ${BUILD_DIR}
 
 coin --cache_folder ${DIR}/cache.dir --to ${BUILD_DIR} deb http://http.us.debian.org/debian/pool/main/u/usbutils/usbutils_007-2_${ARCH_DEB}.deb --subfolder usbutils
-coin --to ${BUILD_DIR} deb http://http.us.debian.org/debian/pool/main/libu/libusb-1.0/libusb-1.0-0_1.0.19-1_${ARCH_DEB}.deb --subfolder libusb
+coin --cache_folder ${DIR}/cache.dir --to ${BUILD_DIR} deb http://http.us.debian.org/debian/pool/main/libu/libusb-1.0/libusb-1.0-0_1.0.19-1_${ARCH_DEB}.deb --subfolder libusb
 
 echo "zipping"
 rm -rf ${NAME}*.tar.gz
