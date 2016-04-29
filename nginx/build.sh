3af6e588e379f98ba942937630bebd7ffbf3bd65
Boris Rybalkin <ribalkin@gmail.com>
2016-04-29 21:23:54 +0100

    more flags

--- nginx/build.sh
+++ nginx/build.sh
@@ -14,7 +14,7 @@ export TMPDIR=/tmp
 export TMP=/tmp
 NAME=nginx
 VERSION=1.8.0
-OPENSSL_VERSION=1.0.1e
+OPENSSL_VERSION=1.0.2g
 ROOT=/opt/app/platform
 PREFIX=${ROOT}/${NAME}
 
@@ -32,15 +32,11 @@ tar xzf ${NAME}-${VERSION}.tar.gz
 wget http://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz
 tar xzf openssl-${OPENSSL_VERSION}.tar.gz
 
-cd openssl-${OPENSSL_VERSION}
-
-sed -i 's/install: all install_docs install_sw/install: all install_sw/g' Makefile.org
-./config
-
-cd ..
-
 cd ${NAME}-${VERSION}
 ./configure --prefix=${PREFIX} --with-cc-opt="-static -static-libgcc" --with-ld-opt="-static" --with-http_ssl_module --with-http_gzip_static_module --with-http_spdy_module --with-openssl=../openssl-${OPENSSL_VERSION}
+
+sed -i "/CFLAGS/s/ \-O //g" objs/Makefile
+
 make -j1
 rm -rf ${PREFIX}
 make install_sw
