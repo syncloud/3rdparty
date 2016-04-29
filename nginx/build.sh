042c350a63f66bca71ca19d484edac026dcc7dd1
Boris Rybalkin <ribalkin@gmail.com>
2016-04-29 08:45:43 +0100

    trying static build with openssl

--- nginx/build.sh
+++ nginx/build.sh
@@ -14,6 +14,7 @@ export TMPDIR=/tmp
 export TMP=/tmp
 NAME=nginx
 VERSION=1.8.0
+OPENSSL_VERSION=1.0.2g
 ROOT=/opt/app/platform
 PREFIX=${ROOT}/${NAME}
 
@@ -27,12 +28,18 @@ cd build
 
 wget http://nginx.org/download/${NAME}-${VERSION}.tar.gz --progress dot:giga
 tar xzf ${NAME}-${VERSION}.tar.gz
+
+wget http://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz
+tar xzf openssl-${OPENSSL_VERSION}.tar.gz
+
 cd ${NAME}-${VERSION}
-./configure --prefix=${PREFIX} --with-http_ssl_module --with-http_gzip_static_module --with-http_spdy_module 
-make -j2
+./configure --prefix=${PREFIX} --with-cc-opt="-static -static-libgcc" --with-ld-opt="-static" --with-http_ssl_module --with-http_gzip_static_module --with-http_spdy_module --with-openssl=../openssl-${OPENSSL_VERSION}
+make -j1
 rm -rf ${PREFIX}
 make install
 cd ../..
+
+ldd ${PREFIX}/sbin/nginx
 
 rm -rf ${NAME}-${ARCH}.tar.gz
 tar cpzf ${NAME}-${ARCH}.tar.gz -C ${ROOT} ${NAME}
