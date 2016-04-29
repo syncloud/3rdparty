512faaeb23c6a123eff1ac6aa9ac1fb2ece719e5
Boris Rybalkin <ribalkin@gmail.com>
2016-04-29 21:46:35 +0100

    embed pcre

--- nginx/build.sh
+++ nginx/build.sh
@@ -15,12 +15,13 @@ export TMP=/tmp
 NAME=nginx
 VERSION=1.8.0
 OPENSSL_VERSION=1.0.2g
+PCRE_VERSION=8.34
 ROOT=/opt/app/platform
 PREFIX=${ROOT}/${NAME}
 
 echo "building ${NAME}"
 
-apt-get -y install build-essential flex bison libreadline-dev zlib1g-dev libpcre3-dev
+apt-get -y install build-essential flex bison libreadline-dev zlib1g-dev
 
 rm -rf build
 mkdir -p build
@@ -32,8 +33,11 @@ tar xzf ${NAME}-${VERSION}.tar.gz
 wget http://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz
 tar xzf openssl-${OPENSSL_VERSION}.tar.gz
 
+wget http://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-${PCRE_VERSION}
+tar xzf pcre-${PCRE_VERSION}.tar.gz
+
 cd ${NAME}-${VERSION}
-./configure --prefix=${PREFIX} --with-cc-opt="-static -static-libgcc" --with-ld-opt="-static" --with-http_ssl_module --with-http_gzip_static_module --with-http_spdy_module --with-openssl=../openssl-${OPENSSL_VERSION}
+./configure --prefix=${PREFIX} --with-cc-opt="-static -static-libgcc" --with-ld-opt="-static" --with-http_ssl_module --with-http_gzip_static_module --with-http_spdy_module --with-openssl=../openssl-${OPENSSL_VERSION} --with-pcre=../pcre-${PCRE_VERSION}
 
 sed -i "/CFLAGS/s/ \-O //g" objs/Makefile
 
