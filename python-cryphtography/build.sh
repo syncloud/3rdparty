4983bb6ec31bc028c4683262bc95c46994ff7cf3
Boris Rybalkin <ribalkin@gmail.com>
2016-05-15 22:25:10 +0100

    python cryptography for letsencrypt/certbot

---
+++ python-cryphtography/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd ${DIR}
+
+OPENSSL_VERSION=1.0.2h
+
+rm -rf openssl-${OPENSSL_VERSION}
+curl -O https://openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz
+tar xvf openssl-${OPENSSL_VERSION}.tar.gz
+cd openssl-${OPENSSL_VERSION}
+./config no-shared no-ssl2 -fPIC --prefix=${DIR}/openssl
+make && make install
+cd ..
+
+CFLAGS="-I${DIR}/openssl/include" LDFLAGS="-L${DIR}/openssl/lib" pip wheel --no-use-wheel cryptography
+
+cp ${DIR}/dist/*.whl ${DIR}/
