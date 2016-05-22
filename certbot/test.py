0c55eac40e394d07b6da2f2e64b8482fe4e7f4b8
Boris Rybalkin <ribalkin@gmail.com>
2016-05-22 07:53:09 +0100

    more help

--- certbot/test.py
+++ certbot/test.py
@@ -1,10 +1,11 @@
 #!build/python/bin/python
 
-from certbot import main
-from certbot.plugins import disco as plugins_disco
 from subprocess import check_call
 
-
-print(plugins_disco.PluginsRegistry.find_all())
-
 check_call('build/bin/certbot -h', shell=True)
+
+check_call('build/bin/certbot -h certonly', shell=True)
+
+check_call('build/bin/certbot -h install', shell=True)
+
+check_call('build/bin/certbot plugins', shell=True)
