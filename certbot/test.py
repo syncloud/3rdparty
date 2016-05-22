e26fcff3fa243fbd26bb42c87c65a23a0449cc81
Boris Rybalkin <ribalkin@gmail.com>
2016-05-22 19:57:20 +0100

    fix typo

--- certbot/test.py
+++ certbot/test.py
@@ -2,6 +2,6 @@
 
 from subprocess import check_call
 
-check_call('build/bin/certbot --help all, shell=True)
+check_call('build/bin/certbot --help all', shell=True)
 
 check_call('build/bin/certbot plugins', shell=True)
