dec413382eb7b48e3f3e7495d91715fb14568e6a
Boris Rybalkin <ribalkin@gmail.com>
2016-06-03 08:49:47 +0100

    fix test

--- certbot/test.py
+++ certbot/test.py
@@ -1,7 +1,7 @@
-#!build/python/bin/python
+#!build/certbot/python/bin/python
 
 from subprocess import check_call
 
-check_call('build/bin/certbot --help all', shell=True)
+check_call('build/certbot/bin/certbot --help all', shell=True)
 
-check_call('build/bin/certbot plugins', shell=True)
+check_call('build/certbot/bin/certbot plugins', shell=True)
