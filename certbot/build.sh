6014b19977f3b21672758a5248c04b9b0446588a
Boris Rybalkin <ribalkin@gmail.com>
2016-06-06 23:14:55 +0100

    overriding default cryptography lib with custom build (with static open ssl build)

--- certbot/build.sh
+++ certbot/build.sh
@@ -15,9 +15,15 @@ LIB_DIR=${BUILD_DIR}/lib
 TEST_DIR=${DIR}/test
 DOWNLOAD_URL=http://build.syncloud.org:8111/guestAuth/repository/download
 PYPI_URL=https://pypi.python.org/packages
+PYTHON_SITE_PACKAGES_DIR=${BUILD_DIR}/${NAME}/python/lib/python2.7/site-packages
 
-apt-get update
-apt-get install -y libffi-dev
+if [[ $EUID -ne 0 ]]; then
+   echo "non root, skipping apt dependencies"
+else
+   apt-get update
+   apt-get install -y libffi-dev
+fi
+
 rm -rf ${BUILD_DIR}
 mkdir -p ${BUILD_DIR}/${NAME}
 
@@ -29,6 +35,11 @@ rm -rf .coin.cache/*_python-*/output
 coin --to ${BUILD_DIR}/${NAME} raw ${DOWNLOAD_URL}/thirdparty_python_${ARCH}/lastSuccessful/python-${ARCH}.tar.gz
 
 ${BUILD_DIR}/${NAME}/python/bin/pip install certbot
+
+coin --to=${PYTHON_SITE_PACKAGES_DIR} py ${DOWNLOAD_URL}/thirdparty_python_cryptography_${ARCH}/lastSuccessful/cryptography-1.3.2-cp27-none-linux_${ARCH}.whl
+rm -rf ${PYTHON_SITE_PACKAGES_DIR}/cryptography
+mv ${PYTHON_SITE_PACKAGES_DIR}/cryptography-1.3.2/cryptography ${PYTHON_SITE_PACKAGES_DIR}/cryptography
+rm -rf ${PYTHON_SITE_PACKAGES_DIR}/cryptography-1.3.2
 
 rm -rf ${DIR}/${NAME}-${ARCH}.tar.gz
 tar czf ${DIR}/${NAME}-${ARCH}.tar.gz -C ${BUILD_DIR} .
--- certbot/test.py
+++ certbot/test.py
@@ -4,4 +4,4 @@ from subprocess import check_call
 
 check_call('build/certbot/bin/certbot --help all', shell=True)
 
-check_call('build/certbot/bin/certbot plugins', shell=True)
+check_call('build/certbot/bin/certbot plugins --logs-dir=. --config-dir=.', shell=True)
