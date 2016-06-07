cf856f615bc9943321dbd75a70271bbbe475ea2c
Boris Rybalkin <ribalkin@gmail.com>
2016-06-07 23:27:16 +0100

    trying to embedd openssl

--- python/build.sh
+++ python/build.sh
@@ -16,18 +16,24 @@ export TMPDIR=/tmp
 export TMP=/tmp
 
 NAME=python
-PREFIX=${DIR}/build/${NAME}
+BUILD_DIR=${DIR}/build
+PREFIX=${BUILD_DIR}/${NAME}
+OPENSSL_ROOT=${BUILD_DIR}/openssl
 
 apt-get update
 apt-get -y install build-essential flex bison libreadline-dev zlib1g-dev libpcre3-dev libssl-dev libbz2-dev
 
-rm -rf build
-mkdir build
-cd ${DIR}/build
+rm -rf ${BUILD_DIR}
+mkdir ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+${DIR}/build-openssl.sh
+
 wget https://github.com/yyuu/pyenv/archive/master.zip
 unzip master.zip
-export PYTHON_CONFIGURE_OPTS="--enable-shared --enable-unicode=ucs4"
-./pyenv-master/plugins/python-build/bin/python-build ${VERSION} ${PREFIX}
+#export PYTHON_CONFIGURE_OPTS="CPPFLAGS=-I${OPENSSL_ROOT}/include LDFLAGS=-L${OPENSSL_ROOT}/lib"
+export PYTHON_CONFIGURE_OPTS="CPPFLAGS=-I${OPENSSL_ROOT}/include LDFLAGS=-L${OPENSSL_ROOT}/lib --enable-shared --enable-unicode=ucs4"
+${BUILD_DIR}/pyenv-master/plugins/python-build/bin/python-build --verbose ${VERSION} ${PREFIX}
 
 mv ${PREFIX}/bin/python ${PREFIX}/bin/python.bin
 cp ${DIR}/python ${PREFIX}/bin/
