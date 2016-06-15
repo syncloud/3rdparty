55a24bebbd2c27e946e34a3a4ff2d8cb61ab298d
Boris Rybalkin <ribalkin@gmail.com>
2016-06-15 22:46:46 +0100

    adding ld path

--- certbot/test.py
+++ certbot/test.py
@@ -9,9 +9,9 @@ BUILD_DIR = join(DIR, 'build')
 ROOTFS_DIR = join(BUILD_DIR, 'rootfs')
 
 shutil.copytree(join(BUILD_DIR, 'certbot'), join(ROOTFS_DIR, 'certbot'))
+prefix = 'chroot {0} export LD_LIBRARY_PATH=certbot/python/lib'.format(ROOTFS_DIR)
+check_call('{0} ldd certbot/python/lib/python2.7/lib-dynload/_ssl.so'.format(prefix), shell=True)
 
-check_call('chroot {0} ldd certbot/python/lib/python2.7/lib-dynload/_ssl.so'.format(ROOTFS_DIR), shell=True)
+check_call('{0} certbot/bin/certbot --help all'.format(prefix), shell=True)
 
-check_call('chroot {0} certbot/bin/certbot --help all'.format(ROOTFS_DIR), shell=True)
-
-check_call('chroot {0} certbot/bin/certbot plugins --logs-dir=. --config-dir=.'.format(ROOTFS_DIR), shell=True)
+check_call('{0} certbot/bin/certbot plugins --logs-dir=. --config-dir=.'.format(prefix), shell=True)
