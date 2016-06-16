96e1592c0f8f007554b0c10229a432e5f56091d9
Boris Rybalkin <ribalkin@gmail.com>
2016-06-16 07:38:59 +0100

    fix LD path again

--- certbot/test.py
+++ certbot/test.py
@@ -3,13 +3,16 @@
 from subprocess import check_call
 import shutil
 from os.path import dirname, join
+import os
+
 
 DIR = dirname(__file__)
 BUILD_DIR = join(DIR, 'build')
 ROOTFS_DIR = join(BUILD_DIR, 'rootfs')
 
 shutil.copytree(join(BUILD_DIR, 'certbot'), join(ROOTFS_DIR, 'certbot'))
-prefix = 'chroot {0} LD_LIBRARY_PATH=certbot/python/lib'.format(ROOTFS_DIR)
+os.environ['LD_LIBRARY_PATH'] ='certbot/python/lib'
+prefix = 'chroot {0}'.format(ROOTFS_DIR)
 check_call('{0} ldd certbot/python/lib/python2.7/lib-dynload/_ssl.so'.format(prefix), shell=True)
 
 check_call('{0} certbot/bin/certbot --help all'.format(prefix), shell=True)
