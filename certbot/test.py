b62c94f7d907d2a1d2fb0a260c9ff04ca4b410a6
Boris Rybalkin <ribalkin@gmail.com>
2016-06-16 07:25:06 +0100

    fixing LD path

--- certbot/test.py
+++ certbot/test.py
@@ -9,7 +9,7 @@ BUILD_DIR = join(DIR, 'build')
 ROOTFS_DIR = join(BUILD_DIR, 'rootfs')
 
 shutil.copytree(join(BUILD_DIR, 'certbot'), join(ROOTFS_DIR, 'certbot'))
-prefix = 'chroot {0} export LD_LIBRARY_PATH=certbot/python/lib'.format(ROOTFS_DIR)
+prefix = 'chroot {0} LD_LIBRARY_PATH=certbot/python/lib'.format(ROOTFS_DIR)
 check_call('{0} ldd certbot/python/lib/python2.7/lib-dynload/_ssl.so'.format(prefix), shell=True)
 
 check_call('{0} certbot/bin/certbot --help all'.format(prefix), shell=True)
