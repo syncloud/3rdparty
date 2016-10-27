4fcdd1eb0656c332d98b0d2657313aadf8d92acf
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-10-26 23:59:05 -0400

    Building git specifically for gogs

--- git/build.sh
+++ git/build.sh
@@ -12,16 +12,19 @@ ARCH=$1
 
 NAME=git
 BUILD_DIR=${DIR}/build
-PREFIX=${BUILD_DIR}/${NAME}
+PREFIX_APP=/opt/app/gogs
+PREFIX=${PREFIX_APP}/git
 VERSION=2.10.1
 
 sudo apt-get -y install libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev
 sudo apt-get -y install asciidoc xmlto docbook2x
 sudo apt-get -y install autoconf
 
-rm -rf ${PREFIX}
-mkdir -p ${PREFIX}
+rm -rf ${BUILD_DIR}
+mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
+
+rm -rf ${PREFIX_APP}
 
 wget https://github.com/git/git/archive/v${VERSION}.tar.gz -O v${VERSION}.tar.gz
 rm -rf git-${VERSION}
@@ -32,8 +35,8 @@ make configure
 ./configure --prefix=${PREFIX}
 
 make all doc info
-make install install-doc install-html install-info
+make install
 
 cd ${DIR}
 rm -rf ${NAME}-${ARCH}.tar.gz
-tar cpzf ${NAME}-${ARCH}.tar.gz -C ${BUILD_DIR} ${NAME}
+tar cpzf ${NAME}-${ARCH}.tar.gz -C ${PREFIX_APP} ${NAME}
