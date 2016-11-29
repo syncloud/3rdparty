639fa755af9280b7e9c7cc9c9a87299e23e4decd
Boris Rybalkin <ribalkin@gmail.com>
2016-11-29 17:00:46 +0000

    snapd: fixing binary build

--- snapd/build.sh
+++ snapd/build.sh
@@ -5,12 +5,15 @@ cd ${DIR}
 
 export GOPATH=${DIR}
 export PATH=${PATH}:${GOPATH}/bin
+BUILD_DIR=${DIR}/build
+
 go get -d -v github.com/snapcore/snapd/...
 cd src/github.com/snapcore/snapd
 go get -u github.com/kardianos/govendor
 govendor sync
-./run-checks
+#./run-checks
 
-rm -rf build
-mkdir build
-go build -o build/snapd github.com/snapcore/snapd/cmd/snapd
+cd ${DIR}
+rm -rf ${BUILD_DIR}
+mkdir ${BUILD_DIR}
+go build -o ${BUILD_DIR}/snapd github.com/snapcore/snapd/cmd/snapd
