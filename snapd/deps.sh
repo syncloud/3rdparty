ac5ee3b2b276486a82e4bd4ec14ac8121f400f32
Boris Rybalkin <ribalkin@gmail.com>
2016-12-24 12:54:41 +0000

    snapd: deps: libudev-dev

--- snapd/deps.sh
+++ snapd/deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install libusb-dev golang-1.6 build-essential autoconf libglib2.0-dev libseccomp-dev libapparmor-dev python-docutils
+sudo apt-get install libusb-dev golang-1.6 build-essential autoconf libglib2.0-dev libseccomp-dev libapparmor-dev python-docutils libudev-dev
 ln -s /usr/lib/go-1.6/bin/go /usr/bin/go
