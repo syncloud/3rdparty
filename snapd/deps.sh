2e70224b48a628a854a95b731a5de65132e2f142
Boris Rybalkin <ribalkin@gmail.com>
2016-12-30 08:51:18 +0000

    snapd: force install deps

--- snapd/deps.sh
+++ snapd/deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install libusb-dev golang-1.6 build-essential autoconf libglib2.0-dev libseccomp-dev libapparmor-dev python-docutils libudev-dev squashfs-tools
+sudo apt-get install -y libusb-dev golang-1.6 build-essential autoconf libglib2.0-dev libseccomp-dev libapparmor-dev python-docutils libudev-dev squashfs-tools
 ln -s /usr/lib/go-1.6/bin/go /usr/bin/go
