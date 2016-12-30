29ad709de09e614382dd598615722143c9641eab
Boris Rybalkin <ribalkin@gmail.com>
2016-12-30 08:59:41 +0000

    snapd: fix go imstall

--- snapd/deps.sh
+++ snapd/deps.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sudo apt-get install -y libusb-dev golang-1.6 build-essential autoconf libglib2.0-dev libseccomp-dev libapparmor-dev python-docutils libudev-dev squashfs-tools
+rm /usr/bin/go
 ln -s /usr/lib/go-1.6/bin/go /usr/bin/go
