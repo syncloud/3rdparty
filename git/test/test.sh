731a4a32a2683ab1191ee14c1774f65483d2c8b9
Boris Rybalkin <ribalkin@gmail.com>
2017-02-06 18:02:21 +0000

    fix path

--- git/test/test.sh
+++ git/test/test.sh
@@ -19,7 +19,7 @@ rm -rf ${ROOTFS}
 mkdir ${ROOTFS}
 tar xzf rootfs.tar.gz -C ${ROOTFS}
 
-tar xzvf ${DIR}/../${NAME}-${ARCH}.tar.gz -C ${ROOTFS}
+tar xzvf ${DIR}/../git-${ARCH}.tar.gz -C ${ROOTFS}
 
-chroot ${ROOTFS}/${NAME}/git/bin/git config -l
+chroot ${ROOTFS}/git/bin/git config -l
 
