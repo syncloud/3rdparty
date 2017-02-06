80e6a0642609b0bc57cdf31656504ab973ec7d39
Boris Rybalkin <ribalkin@gmail.com>
2017-02-06 15:32:38 +0000

    fix test

--- git/test/test.sh
+++ git/test/test.sh
@@ -3,8 +3,9 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
-NAME=gogs
+NAME=git
 ARCH=$(dpkg-architecture -q DEB_HOST_GNU_CPU)
+ROOTFS=${DIR}/.rootfs
 
 if [ ! -f "rootfs.tar.gz" ]; then
     wget http://build.syncloud.org:8111/guestAuth/repository/download/${DISTRO}_rootfs_syncloud_${ARCH}/lastSuccessful/rootfs.tar.gz\
