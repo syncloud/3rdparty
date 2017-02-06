78b481ce3870214a8eac42bcb3f93c34bd755aa3
Boris Rybalkin <ribalkin@gmail.com>
2017-02-06 08:51:50 +0000

    chroot test

--- git/test.sh
+++ git/test.sh
@@ -6,4 +6,18 @@ cd ${DIR}
 NAME=gogs
 ARCH=$(dpkg-architecture -q DEB_HOST_GNU_CPU)
 
-tar cpzf ${NAME}-${ARCH}.tar.gz -C ${BUILD_DIR} ${NAME}
+if [ ! -f "rootfs.tar.gz" ]; then
+    wget http://build.syncloud.org:8111/guestAuth/repository/download/${DISTRO}_rootfs_syncloud_${ARCH}/lastSuccessful/rootfs.tar.gz\
+  -O rootfs.tar.gz --progress dot:giga
+else
+    echo "rootfs.tar.gz is here"
+fi
+
+rm -rf ${ROOTFS}
+mkdir ${ROOTFS}
+tar xzf rootfs.tar.gz -C ${ROOTFS}
+
+tar xzvf ${NAME}-${ARCH}.tar.gz -C ${ROOTFS}
+
+chroot ${ROOTFS}/${NAME}/git/bin/git config -l
+
