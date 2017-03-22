fd9e2162c5a11e2cdfa88eaef4a0bcccba4e1041
Boris Rybalkin <ribalkin@gmail.com>
2017-03-22 18:34:40 +0000

    fix copy all to rootfs

--- tools/run-in-chroot.sh
+++ tools/run-in-chroot.sh
@@ -9,13 +9,6 @@ ARCH=$1
 ROOTFS=rootfs
 #ARCH=$(dpkg-architecture -q DEB_HOST_GNU_CPU)
 
-if [ ! -f rootfs-${ARCH}.tar.gz ]; then
-  wget http://build.syncloud.org:8111/guestAuth/repository/download/debian_rootfs_syncloud_${ARCH}/lastSuccessful/rootfs.tar.gz\
-  -O rootfs-${ARCH}.tar.gz --progress dot:giga
-else
-  echo "skipping rootfs"
-fi
-
 function cleanup {
     mount | grep rootfs
     mount | grep rootfs | awk '{print "umounting "$1; system("umount "$3)}'
@@ -28,6 +21,16 @@ function cleanup {
 
 cleanup || true
 
+mkdir tmp
+cp * tmp/
+
+if [ ! -f rootfs-${ARCH}.tar.gz ]; then
+  wget http://build.syncloud.org:8111/guestAuth/repository/download/debian_rootfs_syncloud_${ARCH}/lastSuccessful/rootfs.tar.gz\
+  -O rootfs-${ARCH}.tar.gz --progress dot:giga
+else
+  echo "skipping rootfs"
+fi
+
 echo "extracting rootfs"
 mkdir -p ${ROOTFS}
 tar xzf rootfs-${ARCH}.tar.gz -C ${ROOTFS}
@@ -36,7 +39,7 @@ chroot ${ROOTFS} /bin/bash -c "mount -t devpts devpts /dev/pts"
 chroot ${ROOTFS} /bin/bash -c "mount -t proc proc /proc"
 
 mkdir ${ROOTFS}/build
-cp * ${ROOTFS}/build/
+cp tmp/* ${ROOTFS}/build/
 
 if [ -f ${ROOTFS}/build/deps.sh ]; then
     chroot ${ROOTFS} /build/deps.sh
