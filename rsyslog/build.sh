8d6c71124502b7175a96e557af492d83ef3dbbf4
Boris Rybalkin <ribalkin@gmail.com>
2017-03-22 21:59:03 +0000

    fix rsyslog deps

--- rsyslog/build.sh
+++ rsyslog/build.sh
@@ -17,7 +17,7 @@ VERSION=8.16.0
 BUILD=${DIR}/build
 PREFIX=${BUILD}/${NAME}
 
-apt-get -y install build-essential cmake libncurses5-dev libestr-dev libjson-c-dev uuid-dev libgcrypt20-dev liblogging-stdlog-dev
+apt-get -y install build-essential cmake libncurses5-dev libestr-dev libjson-c-dev uuid-dev libgcrypt20-dev liblogging-stdlog-dev pkg-config
 
 rm -rf ${BUILD}
 mkdir ${BUILD}
