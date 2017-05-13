03315ad8d9c8ed344ffe07ca79d046445ac8aea2
Boris Rybalkin <ribalkin@gmail.com>
2017-05-13 08:13:12 +0100

    apt update

--- ImageMagick/build.sh
+++ ImageMagick/build.sh
@@ -18,7 +18,7 @@ ROOT=${DIR}/build
 PREFIX=${ROOT}/${NAME}
 
 echo "building ${NAME}"
-
+apt-get update
 apt-get -y install build-essential \
     libxml2-dev autoconf libjpeg-dev libpng12-dev libfreetype6-dev \
     libzip-dev zlib1g-dev libcurl4-gnutls-dev dpkg-dev \
