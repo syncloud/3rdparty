9e37c2faab80d6df0859f8b38d7672c8e35c1112
Boris Rybalkin <ribalkin@gmail.com>
2017-05-14 17:09:34 +0100

    less debug

--- tools/upload.sh
+++ tools/upload.sh
@@ -11,6 +11,5 @@ if [ -z "$1" ]; then
 fi
 
 echo "$ARTIFACT_SSH_KEY" | base64 --decode > artifact_ssh.key
-cat artifact_ssh.key
 chmod 600 artifact_ssh.key
 scp -oStrictHostKeyChecking=no -i artifact_ssh.key $1 artifact@artifact.syncloud.org:/home/artifact/repo/3rdparty/$1
