400cc0438f707954ae838a142f6b2b7fad3ad5e5
Boris Rybalkin <ribalkin@gmail.com>
2017-05-18 18:53:05 +0100

    use matrix definition to trigger build as opposed to last git commit changes

--- build.sh
+++ build.sh
@@ -4,15 +4,15 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
 apt-get -qq update
-apt-get -qqy install git openssh-client wget > /dev/null
+apt-get -qqy install openssh-client wget > /dev/null
 
 PROJECT=$1
 ARCH=$(uname -m)
-if git diff-tree --name-only HEAD^..HEAD | grep ${PROJECT}; then
+#if git diff-tree --name-only HEAD^..HEAD | grep ${PROJECT}; then
     echo "${PROJECT}: building"
     ${DIR}/${PROJECT}/build.sh ${ARCH}
     cd ${PROJECT}
     ${DIR}/tools/upload.sh ${PROJECT}-${ARCH}.tar.gz
-else
-    echo "${PROJECT} skipping, no changes in last commit"
-fi
+#else
+#    echo "${PROJECT} skipping, no changes in last commit"
+#fi
