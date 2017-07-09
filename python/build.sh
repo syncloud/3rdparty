f98596ba32e8b11c0b0040f0c6e772188875a951
Boris Rybalkin <ribalkin@gmail.com>
2017-07-10 00:24:53 +0100

    upgrade openssl for python

--- python/build.sh
+++ python/build.sh
@@ -10,44 +10,48 @@ fi
 
 ARCH=$1
 
-VERSION=2.7.10
+VERSION=2.7.13
 
 export TMPDIR=/tmp
 export TMP=/tmp
 
 NAME=python
 PREFIX=${DIR}/build/${NAME}
+OPENSSL=${DIR}/build/openssl
 
-apt-get update
-apt-get -y install build-essential flex bison libreadline-dev zlib1g-dev libpcre3-dev libssl-dev libbz2-dev libsqlite3-dev unzip
-
+#apt-get update
+apt-get -y install build-essential flex bison libreadline-dev zlib1g-dev libpcre3-dev libbz2-dev libsqlite3-dev unzip
+apt-get remove libssl-dev
 rm -rf build
 mkdir build
 cd ${DIR}/build
-wget https://github.com/yyuu/pyenv/archive/master.zip
-unzip master.zip
-export PYTHON_CONFIGURE_OPTS="--enable-shared --enable-unicode=ucs4"
-./pyenv-master/plugins/python-build/bin/python-build ${VERSION} ${PREFIX}
+
+wget http://artifact.syncloud.org/3rdparty/openssl-${ARCH}.tar.gz
+tar xf openssl-${ARCH}.tar.gz
+
+# DANGEROUS to run outside of drone/docker
+cp --remove-destination -r ${OPENSSL}/lib/*.so* /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/
+cp --remove-destination -r ${OPENSSL}/include/openssl /usr/include/
+
+wget https://www.python.org/ftp/python/${VERSION}/Python-${VERSION}.tar.xz
+tar xf Python-${VERSION}.tar.xz
+cd Python-${VERSION}
+
+#CPPFLAGS=-I${OPENSSL}/include \
+#LDFLAGS=-L${OPENSSL}/lib \
+./configure --prefix=${PREFIX} --enable-shared --enable-unicode=ucs4
+make
+make install
 
 mv ${PREFIX}/bin/python ${PREFIX}/bin/python.bin
 cp ${DIR}/python ${PREFIX}/bin/
 
+${PREFIX}/bin/python -m ensurepip --upgrade
 mv ${PREFIX}/bin/pip ${PREFIX}/bin/pip_runner
 cp ${DIR}/pip ${PREFIX}/bin/
 
-cp --remove-destination /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libssl*.so* ${PREFIX}/lib
-
-# ubuntu hack
-if [ -f /lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libssl*.so* ]; then
-  cp --remove-destination /lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libssl*.so* ${PREFIX}/lib
-fi
-
-cp --remove-destination /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libcrypto*.so* ${PREFIX}/lib
-
-# ubuntu hack
-if [ -f /lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libcrypto*.so* ]; then
-    cp --remove-destination /lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libcrypto*.so* ${PREFIX}/lib
-fi
+cp --remove-destination ${OPENSSL}/lib/libssl*.so* ${PREFIX}/lib
+cp --remove-destination ${OPENSSL}/lib/libcrypto*.so* ${PREFIX}/lib
 
 ldd ${PREFIX}/lib/python2.7/lib-dynload/_ssl.so
 
