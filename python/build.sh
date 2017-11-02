1bf70560599f6e92a456e7739e83145a28dd12c9
Boris Rybalkin <ribalkin@gmail.com>
2017-11-02 19:04:51 +0000

    python: unfix cryptography version

--- python/build.sh
+++ python/build.sh
@@ -91,6 +91,6 @@ tar cpzf ${NAME}-${ARCH}.tar.gz -C ${DIR}/build ${NAME}
 ${PREFIX}/bin/python -c 'from urllib2 import urlopen; print(urlopen("https://google.com"))'
 ${PREFIX}/bin/python -c 'import ssl; print(ssl.OPENSSL_VERSION)'
 
-${PREFIX}/bin/pip install cryptography==2.0
+#${PREFIX}/bin/pip install cryptography==2.0
 ${PREFIX}/bin/pip install certbot
 ${PREFIX}/bin/certbot help
