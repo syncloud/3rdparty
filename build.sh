7b65cad05e9684d1740cd8427cb40447753abbea
Boris Rybalkin <ribalkin@gmail.com>
2017-11-08 20:05:17 +0000

    cleanup

--- build.sh
+++ build.sh
@@ -2,9 +2,6 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
-export DEBIAN_FRONTEND=noninteractive
-apt-get -qq update
-apt-get -qqy install openssh-client wget dpkg-dev curl > /dev/null
 
 PROJECT=$1
 ARCH=$(uname -m)
