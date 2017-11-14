73aeaf11ad3a32b866acf1d9e66c1eba4e35ab16
Boris Rybalkin <ribalkin@gmail.com>
2017-11-14 19:00:00 +0000

    do not use install

--- mongodb/build.sh
+++ mongodb/build.sh
@@ -14,9 +14,9 @@ NAME=mongodb
 VERSION=3.0.7
 PREFIX=${DIR}/build/${NAME}
 
-rm -rf $PREFIX
+rm -rf ${DIR}/build
 mkdir -p $PREFIX
-cd $PREFIX
+cd ${DIR}/build
 
 #echo "repackage distro version"
 #apt-get update
@@ -63,39 +63,42 @@ mv /usr/local/lib/python2.7/dist-packages/scons-* /usr/local/lib/python2.7/site-
 #https://gist.github.com/kitsook/f0f53bc7acc468b6e94c
 ls -la src/third_party
 cp $DIR/SConscript src/third_party/v8-3.25/
-rm -rf src/third_party/v8
+
 scons -j 2 --wiredtiger=off --c++11=off --js-engine=v8-3.25 --disable-warnings-as-errors CXXFLAGS="-std=gnu++11" core
-scons --prefix=$PREFIX install
+#scons --prefix=$PREFIX install
+ls -la build/install/bin
+mkdir $PREFIX/bin
+cp build/install/bin/mongod $PREFIX/bin/mongod.bin
 
-ldd bin/mongod.bin
+ldd $PREFIX/bin/mongod.bin
 
-mkdir lib
-cp /usr/lib/libv8.so* lib/
-cp /usr/lib/libsnappy.so* lib/
-cp	 /lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libpcre.so* lib/
-cp /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libpcrecpp.so* lib/
-cp	 /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libssl.so* lib/
-cp 	/usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libcrypto.so* lib/
-cp 	/usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)p/libboost_thread.so* lib/
-cp 	/usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libboost_filesystem.so* lib/
-cp 	/usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libboost_program_options.so* lib/
-cp 	/usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libboost_system.so* lib/
-cp 	/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/librt.so* lib/
-cp 	/usr/lib/libtcmalloc.so* lib/ || true
+mkdir $PREFIX/lib
+cp /usr/lib/libv8.so* $PREFIX/lib/
+cp /usr/lib/libsnappy.so* $PREFIX/lib/
+cp	 /lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libpcre.so* $PREFIX/lib/
+cp /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libpcrecpp.so* $PREFIX/lib/
+cp	 /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libssl.so* $PREFIX/lib/
+cp 	/usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libcrypto.so* $PREFIX/lib/
+cp 	/usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)p/libboost_thread.so* $PREFIX/lib/
+cp 	/usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libboost_filesystem.so* $PREFIX/lib/
+cp 	/usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libboost_program_options.so* $PREFIX/lib/
+cp 	/usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libboost_system.so* $PREFIX/lib/
+cp 	/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/librt.so* $PREFIX/lib/
+cp 	/usr/lib/libtcmalloc.so* $DIR/lib/ || true
 #	libstdc++.so.6 => /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libstdc++.so.6 (0x00007f548ba6d000)
 #	libm.so.6 => /lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libm.so.6 (0x00007f548b76b000)
 #	libgcc_s.so.1 => /lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libgcc_s.so.1 (0x00007f548b555000)
 #	libc.so.6 => /lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libc.so.6 (0x00007f548b1aa000)
 #	/lib64/ld-linux-x86-64.so.2 (0x0000563f87812000)
 #	libdl.so.2 => /lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libdl.so
-cp /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libunwind.so* lib/ || true
-cp /lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/liblzma.so* lib/
+cp /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libunwind.so* $PREFIX/lib/ || true
+cp /lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/liblzma.so* $PREFIX/lib/
 
 export LD_LIBRARY_PATH=${PREFIX}/lib
 
-ldd bin/mongod.bin
+ldd $PREFIX/bin/mongod.bin
 
-./bin/mongod --version
+$PREFIX/bin/mongod --version
 
 cd $DIR
 
