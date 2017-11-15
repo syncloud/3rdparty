e49172de6aa339c417cb11368213ee5cac76ecd1
Boris Rybalkin <ribalkin@gmail.com>
2017-11-15 19:09:54 +0000

    phantomjs v1.9

--- phantomjs/build.sh
+++ phantomjs/build.sh
@@ -10,51 +10,26 @@ fi
 
 ARCH=$1
 
-NAME=mongodb
-VERSION=3.0.7
+NAME=phantomjs
+VERSION=1.9
 PREFIX=${DIR}/build/${NAME}
 
 rm -rf ${DIR}/build
 mkdir -p $PREFIX
 cd ${DIR}/build
 
-echo "building ${NAME}"
-
-ARCHIVE=${NAME}-src-r${VERSION}.tar.gz
-wget https://fastdl.mongodb.org/src/${ARCHIVE} --progress dot:giga
-
-tar xzf ${ARCHIVE}
-cd ${NAME}-src-r${VERSION}
-ls -la
-cat README
-cat docs/building.md
-ls -la src
-
-#http://andyfelong.com/2015/12/mongodb-3-0-7-on-raspberry-pi-2/
 apt-get update
-apt-get -y install libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-thread-dev
-pip install --egg scons==2.3.0
-mv /usr/local/lib/python2.7/dist-packages/scons-* /usr/local/lib/python2.7/site-packages/ | true
+apt-get -y install apt-get install build-essential g++ flex bison gperf ruby perl \ libsqlite3-dev libfontconfig1-dev libicu-dev libfreetype6 libssl-dev \ libpng-dev libjpeg-dev python libx11-dev libxext-dev
 
-#https://gist.github.com/kitsook/f0f53bc7acc468b6e94c
-ls -la src/third_party
-cp $DIR/SConscript src/third_party/v8-3.25/
+git clone git://github.com/ariya/phantomjs.git src
+cd src
+git checkout $VERSION
+git submodule init
+git submodule update
 
-scons -j 2 --wiredtiger=off --c++11=off --js-engine=v8-3.25 --disable-warnings-as-errors CXXFLAGS="-std=gnu++11" core
-scons --prefix=$PREFIX -j 2 --wiredtiger=off --c++11=off --js-engine=v8-3.25 --disable-warnings-as-errors CXXFLAGS="-std=gnu++11" install
-ls -la $PREFIX
-ls -la $PREFIX/bin
-ls -la $PREFIX/lib || true
+python build.py
 
-mv $PREFIX/bin/mongod $PREFIX/bin/mongod.bin
-ldd $PREFIX/bin/mongod.bin
-
-#mkdir $PREFIX/lib
-#export LD_LIBRARY_PATH=${PREFIX}/lib
-#ldd $PREFIX/bin/mongod.bin
-
-cp $DIR/bin/* $PREFIX/bin
-$PREFIX/bin/mongod --version
+cp -r bin $PREFIX
 
 cd $DIR
 
