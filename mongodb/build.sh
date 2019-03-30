086338c904e30915d67d237560178c647bd37d82
Boris Rybalkin <ribalkin@gmail.com>
2019-03-30 09:06:27 +0000

    mongo: try no deps and include libstdc++

--- mongodb/build.sh
+++ mongodb/build.sh
@@ -34,14 +34,11 @@ cat README
 cat docs/building.md
 ls -la src
 
-#http://andyfelong.com/2015/12/mongodb-3-0-7-on-raspberry-pi-2/
-apt-get update
-apt-get -y install libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-thread-dev
+#apt-get update
+#apt-get -y install libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-thread-dev
 pip install scons==2.3.0
 mv /usr/local/lib/python2.7/dist-packages/scons-* /usr/local/lib/python2.7/site-packages/ | true
-#pip2 install -r buildscripts/requirements.txt 
 
-#https://gist.github.com/kitsook/f0f53bc7acc468b6e94c
 ls -la src/third_party
 
 cd src/third_party/mozjs-38/
@@ -49,24 +46,21 @@ cd src/third_party/mozjs-38/
 ./gen-config.sh ${MONGO_ARCH} linux
 cd ../../../
 
-#cp $DIR/SConscript src/third_party/v8-3.25/
-#scons -j 2 --wiredtiger=off --c++11=off --js-engine=v8-3.25 --disable-warnings-as-errors CXXFLAGS="-std=gnu++11" core
-#scons --prefix=$PREFIX -j 2 --wiredtiger=off --c++11=off --js-engine=v8-3.25 --disable-warnings-as-errors CXXFLAGS="-std=gnu++11" install
-#python2 buildscripts/scons.py all
-
 scons -j 2 --wiredtiger=off --mmapv1=on mongod
 scons --prefix=${PREFIX} -j 2 --wiredtiger=off --mmapv1=on install
 
 ls -la ${PREFIX}
 ls -la ${PREFIX}/bin
 ls -la ${PREFIX}/lib || true
+mkdir -p ${PREFIX}/lib
 
 mv ${PREFIX}/bin/mongod ${PREFIX}/bin/mongod.bin
 ldd ${PREFIX}/bin/mongod.bin
 
-#mkdir $PREFIX/lib
-#export LD_LIBRARY_PATH=${PREFIX}/lib
-#ldd $PREFIX/bin/mongod.bin
+cp --remove-destination /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libstdc++.so* ${PREFIX}/lib
+
+export LD_LIBRARY_PATH=${PREFIX}/lib
+ldd ${PREFIX}/bin/mongod.bin
 
 cp ${DIR}/bin/* ${PREFIX}/bin
 ${PREFIX}/bin/mongod --version
