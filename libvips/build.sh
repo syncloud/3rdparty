db0d56076bdb3f72d53d3f605b4412c39e1536a3
Boris Rybalkin <ribalkin@gmail.com>
2019-04-01 22:27:45 +0100

    libvips: fix dep

--- libvips/build.sh
+++ libvips/build.sh
@@ -39,7 +39,7 @@ cp /lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libz.so* ${BUILD_DIR}/lib
 cp /lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libpng12.so* ${BUILD_DIR}/lib
 cp /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libjpeg.so* ${BUILD_DIR}/lib
 cp /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libgmodule-2.0.so* ${BUILD_DIR}/lib
-cp /lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libgobject-2.0.so* ${BUILD_DIR}/lib
+cp /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libgobject-2.0.so* ${BUILD_DIR}/lib
 cp /lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libglib-2.0.so* ${BUILD_DIR}/lib
 cp /lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libexpat.so* ${BUILD_DIR}/lib
 cp /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libstdc++.so* ${BUILD_DIR}/lib
