e5113c98dcb1310d597f66a8fe779b77ee0defe8
Boris Rybalkin <ribalkin@gmail.com>
2020-12-06 12:12:33 +0000

    mongo4: export

--- mongodb-4/build.sh
+++ mongodb-4/build.sh
@@ -22,21 +22,25 @@ PREFIX=${DIR}/build/${NAME}
 rm -rf ${DIR}/build
 mkdir -p $PREFIX
 
+apt update
+apt remove -y gcc cpp
+
 cd ${DIR}/build
 wget --progress=dot:giga https://github.com/syncloud/3rdparty/releases/download/1/gcc-$GCC_VERSION-${ARCH}.tar.gz
 tar xf gcc-$GCC_VERSION-${ARCH}.tar.gz
-export PATH=$DIR/build/gcc-$GCC_VERSION/bin:$PATH
+export CC=$DIR/build/gcc-$GCC_VERSION/bin/gcc
+export CXX=$DIR/build/gcc-$GCC_VERSION/bin/g++
+export PATH=$DIR/build/gcc-$GCC_VERSION/bin:$PATHj
+gcc --version
+$CC --version
+$CXX -version
 
 wget --progress=dot:giga https://github.com/syncloud/3rdparty/releases/download/1/python-${ARCH}.tar.gz
 tar xf python-${ARCH}.tar.gz
 export PATH=$DIR/build/python/bin:$PATH
-
-echo "building ${NAME}"
-apt update
-apt remove -y gcc cpp
-gcc --version
 python --version
 
+echo "building ${NAME}"
 cd ${DIR}/build
 ARCHIVE=mongodb-src-r${VERSION}.tar.gz
 wget https://fastdl.mongodb.org/src/${ARCHIVE} --progress dot:giga
@@ -49,7 +53,7 @@ cat docs/building.md
 ls -la src
 
 pip install -r buildscripts/requirements.txt
-python buildscripts/scons.py --disable-warnings-as-errors -j 2 mongod > build.log || tail -1000 build.log
+python buildscripts/scons.py CC=$CC CXX=$CXX --disable-warnings-as-errors -j 2 mongod > build.log || tail -1000 build.log
 python buildscripts/scons.py --disable-warnings-as-errors --prefix=${PREFIX} -j 2 install
 strip ${PREFIX}/bin/mongo*
 
