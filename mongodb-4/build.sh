d4ab7590647d784d73dc319884c936390732025e
Boris Rybalkin <ribalkin@gmail.com>
2020-12-22 16:30:43 +0000

    mongo4: more debug

--- mongodb-4/build.sh
+++ mongodb-4/build.sh
@@ -112,6 +112,7 @@ ldd ${PREFIX}/bin/mongod.bin
 cp ${DIR}/bin/* ${PREFIX}/bin
 ${PREFIX}/bin/mongod --version
 
+LD_DEBUG=files,libs
 $LD ${PREFIX}/bin/mongod.bin --version || true
 ${PREFIX}/lib/ld.so --verify ${PREFIX}/bin/mongod.bin || true
 ${PREFIX}/lib/ld.so --list ${PREFIX}/bin/mongod.bin --version || true
