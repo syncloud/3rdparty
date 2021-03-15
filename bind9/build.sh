71931b0541b7c90369f04894cb7fdc9c6436c5d0
Boris Rybalkin <ribalkin@gmail.com>
2021-03-15 21:56:32 +0000

    bind9: try -lunwind

--- bind9/build.sh
+++ bind9/build.sh
@@ -25,9 +25,10 @@ apk add -U alpine-sdk linux-headers
 wget ftp://ftp.isc.org/isc/bind9/${VERSION}/bind-${VERSION}.tar.gz
 tar xf bind-${VERSION}.tar.gz
 cd bind-${VERSION}
-CFLAGS="-static" ./configure --prefix=${PREFIX} --without-openssl --disable-symtable
+CFLAGS="-static -lunwind" ./configure --prefix=${PREFIX} --without-openssl --disable-symtable
 make
 make install
 ldd ${PREFIX}/bin/dig
+${PREFIX}/bin/dig --version
 rm -rf ${BUILD}/${NAME}-${ARCH}.tar.gz
 tar czf ${DIR}/${NAME}-${ARCH}.tar.gz -C ${BUILD} ${NAME}
