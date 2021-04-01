2013efb447e45589784aaf201a629021db22fcc6
Boris Rybalkin <ribalkin@gmail.com>
2021-04-01 07:45:26 +0100

    php8: libsasl2-dev

--- php8/build.sh
+++ php8/build.sh
@@ -11,7 +11,7 @@ fi
 ARCH=$1
 
 apt update
-apt install -y libsmbclient-dev libmagickwand-dev libonig-dev cmake libldb-dev libldap2-dev
+apt install -y libsmbclient-dev libmagickwand-dev libonig-dev cmake libldb-dev libldap2-dev libsasl2-dev
 
 export TMPDIR=/tmp
 export TMP=/tmp
