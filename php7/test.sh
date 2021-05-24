f541541baefc360b7bfbffb133f62a9430f2a77c
Boris Rybalkin <ribalkin@gmail.com>
2021-05-24 22:50:15 +0100

    php7: test ini

--- php7/test.sh
+++ php7/test.sh
@@ -7,6 +7,6 @@ PHP=${DIR}/build/php
 export LD_LIBRARY_PATH=${PHP}/lib:${PHP}/lib/private
 ldd ${PHP}/sbin/php-fpm
 
-${PHP}/lib/ld.so ${PHP}/bin/php -i
+${PHP}/lib/ld.so ${PHP}/bin/php -c test.php.ini -i
 
-${PHP}/lib/ld.so ${PHP}/bin/php -i | grep SVG
+${PHP}/lib/ld.so ${PHP}/bin/php -c test.php.ini -i | grep SVG
