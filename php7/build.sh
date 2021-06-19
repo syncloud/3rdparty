2e75473b782e0fa0467b455b2366ed8eb6501404
Boris Rybalkin <ribalkin@gmail.com>
2021-06-19 15:24:41 +0100

    php7: docker build

--- php7/build.sh
+++ php7/build.sh
@@ -7,8 +7,10 @@ ARCH=$(uname -m)
 mkdir -p build/php
 cd build/php
 docker rm php | true
-docker pull php:7.4-cli-alpine
-docker run --name=php php:7.4-cli-alpine php -v
+docker rmi php | true
+docker pull php:7.4-cli
+docker build -t php:syncloud .
+docker run --name=php php:syncloud php -v
 docker export php -o php.tar
 tar xf php.tar
 cp ${DIR}/php.sh bin
