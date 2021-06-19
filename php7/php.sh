013798a72072e62d5c153dd63c5a8ef2078593d4
Boris Rybalkin <ribalkin@gmail.com>
2021-06-19 15:37:18 +0100

    php7: fix ld

--- php7/php.sh
+++ php7/php.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -xe
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-${DIR}/lib/*-linux*/ld-* --library-path ${DIR}/lib/*-linux*:${DIR}/usr/lib/*-linux* ${DIR}/usr/local/bin/php "$@"
+${DIR}/lib/*-linux*/ld-*.so --library-path ${DIR}/lib/*-linux*:${DIR}/usr/lib/*-linux* ${DIR}/usr/local/bin/php "$@"
