a2cc3e293b14c7137820b9a9b266f64825176e69
Boris Rybalkin <ribalkin@gmail.com>
2021-06-19 15:51:14 +0100

    php7: fix libs

--- php7/php.sh
+++ php7/php.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 LIBS=$(echo ${DIR}/lib/*-linux*)
-LIBS=$LIB:$(echo ${DIR}/usr/lib/*-linux*)
+LIBS=$LIBS:$(echo ${DIR}/usr/lib/*-linux*)
 ${DIR}/lib/*-linux*/ld-*.so --library-path $LIBS ${DIR}/usr/local/bin/php "$@"
