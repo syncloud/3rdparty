6a5f8f917d22afe4b67fdc5469a0712658357958
Boris Rybalkin <ribalkin@gmail.com>
2021-06-19 15:27:23 +0100

    php7: fix dir

--- php7/build.sh
+++ php7/build.sh
@@ -4,13 +4,13 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
 ARCH=$(uname -m)
-mkdir -p build/php
-cd build/php
 docker rm php | true
 docker rmi php | true
 docker pull php:7.4-cli
 docker build -t php:syncloud .
 docker run --name=php php:syncloud php -v
+mkdir -p build/php
+cd build/php
 docker export php -o php.tar
 tar xf php.tar
 cp ${DIR}/php.sh bin
