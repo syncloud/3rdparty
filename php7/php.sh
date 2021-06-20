e96e873780a7fb77834f51c8e557955488543ee4
Boris Rybalkin <ribalkin@gmail.com>
2021-06-20 13:06:03 +0100

    php7: config

--- php7/php.sh
+++ php7/php.sh
@@ -3,4 +3,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 LIBS=$(echo ${DIR}/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib/*-linux-gnu*/ImageMagick-6.9.10/modules-Q16/coders)
-PHP_INI_SCAN_DIR=${DIR}/usr/local/etc/php/conf.d ${DIR}/lib/*-linux*/ld-*.so --library-path $LIBS ${DIR}/usr/local/bin/php "$@"
+MAGICK_CONFIGURE_PATH=${DIR}/etc/ImageMagic-6 PHP_INI_SCAN_DIR=${DIR}/usr/local/etc/php/conf.d ${DIR}/lib/*-linux*/ld-*.so --library-path $LIBS ${DIR}/usr/local/bin/php "$@"
