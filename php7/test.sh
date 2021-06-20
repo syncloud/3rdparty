a9a0b5dae9f933e4ea5985d088b658f7712a172c
Boris Rybalkin <ribalkin@gmail.com>
2021-06-20 09:56:25 +0100

    php.ini test

--- php7/test.sh
+++ php7/test.sh
@@ -3,13 +3,16 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
-./build/php/bin/php.sh -i
-./build/php/bin/php.sh -i | grep -i svg
-./build/php/bin/php.sh -i | grep -i smbclient
-./build/php/bin/php.sh -i | grep -i imagemagick
-./build/php/bin/php.sh -i | grep -i gd
-./build/php/bin/php.sh -i | grep -i mysql
-./build/php/bin/php.sh -i | grep -i postgresql
-./build/php/bin/php.sh -i | grep -i ldap
-./build/php/bin/php.sh -r "echo gethostbyname('apps.nextcloud.com');"
-./build/php/bin/php.sh -r "echo gethostbyname('apps.nextcloud.com');" | grep -v apps.nextcloud.com
+echo "extension_dir=\"$DIR/build/pho/usr/local/lib/php/extensions/*\"" > php.ini
+cat php.ini 
+
+./build/php/bin/php.sh -c php.ini -i
+./build/php/bin/php.sh -c php.ini -i | grep -i svg
+./build/php/bin/php.sh -c php.ini -i | grep -i smbclient
+./build/php/bin/php.sh -c php.ini -i | grep -i imagemagick
+./build/php/bin/php.sh -c php.ini -i | grep -i gd
+./build/php/bin/php.sh -c php.ini -i | grep -i mysql
+./build/php/bin/php.sh -c php.ini -i | grep -i postgresql
+./build/php/bin/php.sh -c php.ini -i | grep -i ldap
+./build/php/bin/php.sh -c php.ini -r "echo gethostbyname('apps.nextcloud.com');"
+./build/php/bin/php.sh -c php.ini -r "echo gethostbyname('apps.nextcloud.com');" | grep -v apps.nextcloud.com
