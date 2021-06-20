9e08ffedb4c5ceea7f72e1610d938df8c477fe09
Boris Rybalkin <ribalkin@gmail.com>
2021-06-20 12:59:25 +0100

    php7: do not fail on clean

--- php7/build.sh
+++ php7/build.sh
@@ -4,8 +4,8 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
 ARCH=$(uname -m)
-docker ps -a -q --filter ancestor=php:syncloud --format="{{.ID}}" | xargs docker stop | xargs docker rm
-docker rmi php:syncloud | true
+docker ps -a -q --filter ancestor=php:syncloud --format="{{.ID}}" | xargs docker stop | xargs docker rm || true
+docker rmi php:syncloud || true
 docker pull php:7.4-cli
 docker build -t php:syncloud .
 docker run php:syncloud php -i
