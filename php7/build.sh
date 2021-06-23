67a200e036a7cd7721686cd951d3daa9cc8a0998
Boris Rybalkin <ribalkin@gmail.com>
2021-06-23 22:11:49 +0100

    php7: fix ls

--- php7/build.sh
+++ php7/build.sh
@@ -17,7 +17,7 @@ docker export php -o php.tar
 tar xf php.tar
 rm -rf php.tar
 mv ${BUILD_DIR}/usr/lib/*-linux*/ImageMagick-*/modules-*/coders ${BUILD_DIR}/usr/lib/ImageMagickCoders
-ls -la ${BUILD_DIR}/usr/lib/ImageMagick
+ls -la ${BUILD_DIR}/usr/lib/ImageMagickCoders
 cp ${DIR}/php.sh ${BUILD_DIR}/bin
 cp ${DIR}/php-fpm.sh ${BUILD_DIR}/bin
 mkdir -p ${BUILD_DIR}/lib/php/extensions
