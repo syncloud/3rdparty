74aae1d2a9975e6e01da16c86dcbead38b0c7e4b
Boris Rybalkin <ribalkin@gmail.com>
2021-06-23 22:36:06 +0100

    php7: docker extract (#1)

--- php7/test.sh
+++ php7/test.sh
@@ -3,12 +3,30 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
-PHP=${DIR}/build/php
+find $DIR/build/php -name "*.so"
 
-find ${DIR}/build/php -name "libunwind.so.8"
-find ${DIR}/build/php -name "libc*"
-${PHP}/lib/ld.so --library-path ${PHP}/lib:${PHP}/lib/private ${PHP}/bin/php -i
+echo "extension_dir=\"$DIR/build/php/lib/php/extensions\"" > php.ini
 
-${PHP}/lib/ld.so --library-path ${PHP}/lib:${PHP}/lib/private ${PHP}/bin/php -i | grep SVG
-${PHP}/lib/ld.so --library-path ${PHP}/lib:${PHP}/lib/private ${PHP}/bin/php -r "echo gethostbyname('apps.nextcloud.com');"
-${PHP}/lib/ld.so --library-path ${PHP}/lib:${PHP}/lib/private ${PHP}/bin/php -r "echo gethostbyname('apps.nextcloud.com');" | grep -v apps.nextcloud.com
+./build/php/bin/php.sh -c php.ini -i
+./build/php/bin/php.sh -c php.ini -m
+./build/php/bin/php.sh -c php.ini -i | grep -i svg
+./build/php/bin/php.sh -c php.ini -m | grep -i smbclient
+./build/php/bin/php.sh -c php.ini -m | grep -i intl
+./build/php/bin/php.sh -c php.ini -m | grep -i gmp
+./build/php/bin/php.sh -c php.ini -m | grep -i imagick
+./build/php/bin/php.sh -c php.ini -m | grep -i gd
+./build/php/bin/php.sh -c php.ini -m | grep -i pdo_mysql
+./build/php/bin/php.sh -c php.ini -m | grep -i pdo_pgsql
+./build/php/bin/php.sh -c php.ini -m | grep -i ldap
+./build/php/bin/php.sh -c php.ini -m | grep -i zip
+./build/php/bin/php.sh -c php.ini -m | grep -i memcached
+./build/php/bin/php.sh -c php.ini -m | grep -i apcu
+./build/php/bin/php.sh -c php.ini -m | grep -i mcrypt
+./build/php/bin/php.sh -c php.ini -m | grep -i opcache
+./build/php/bin/php.sh -c php.ini -m | grep -i exif
+./build/php/bin/php.sh -c php.ini -m | grep -i pcntl
+./build/php/bin/php.sh -c php.ini -r "echo gethostbyname('apps.nextcloud.com');"
+./build/php/bin/php.sh -c php.ini -r "echo gethostbyname('apps.nextcloud.com');" | grep -v apps.nextcloud.com
+
+./build/php/bin/php-fpm.sh -c php.ini -i
+./build/php/bin/php-fpm.sh -c php.ini -i | grep -i svg
