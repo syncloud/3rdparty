ec7e15d4293a5c3d91a7015381037d443e77341b
Boris Rybalkin <ribalkin@gmail.com>
2021-07-17 23:02:07 +0100

    python3: fix bin on arm64

--- python3/build.sh
+++ python3/build.sh
@@ -12,7 +12,7 @@ SQLITE_VERSION=autoconf-3310100
 PREFIX=${DIR}/build/python
 
 apt update
-apt -y install build-essential flex bison libreadline-dev zlib1g-dev libpcre3-dev libbz2-dev libsqlite3-dev unzip libffi-dev wget curl
+apt -y install build-essential flex bison libreadline-dev zlib1g-dev libpcre3-dev libbz2-dev libsqlite3-dev unzip libffi-dev wget curl ca-certificates
 
 rm -rf build
 mkdir build
