125608d64dbdc60f819c093bb95c1ae920f8122f
Boris Rybalkin <ribalkin@gmail.com>
2021-08-23 19:24:15 +0100

    gptfdisk: no ldd test

--- gptfdisk/test.sh
+++ gptfdisk/test.sh
@@ -4,5 +4,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
 export LD_LIBRARY_PATH=${DIR}/build/gptfdisk/lib
-ldd ${DIR}/build/gptfdisk/bin/gdisk
+#ldd ${DIR}/build/gptfdisk/bin/gdisk
 ${DIR}/build/gptfdisk/bin/gdisk --help
