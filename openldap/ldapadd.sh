a76a586ba6b389c87171dbc704cf786bcfc21667
Boris Rybalkin <ribalkin@gmail.com>
2021-08-24 19:06:17 +0100

    openldap: fix path

--- openldap/ldapadd.sh
+++ openldap/ldapadd.sh
@@ -3,4 +3,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 LIBS=$(echo ${DIR}/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib)
-${DIR}/lib/*-linux*/ld-*.so --library-path $LIBS ${DIR}/bin/ldapadd "$@"
+${DIR}/lib/*-linux*/ld-*.so --library-path $LIBS ${DIR}/usr/bin/ldapadd "$@"
