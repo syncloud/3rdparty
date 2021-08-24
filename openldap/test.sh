b62133b163549238a37e20e34993759ecced4af3
Boris Rybalkin <ribalkin@gmail.com>
2021-08-24 19:13:04 +0100

    openldap: fix etc dir

--- openldap/test.sh
+++ openldap/test.sh
@@ -12,7 +12,7 @@ rm -rf ${DIR}/build/data
 mkdir ${DIR}/build/slapd.d
 mkdir ${DIR}/build/data
 
-sed -i "s#@ETC_DIR@#${DIR}/build/openldap/etc/openldap#g" ${DIR}/slapd.test.config.ldif
+sed -i "s#@ETC_DIR@#${DIR}/build/openldap/etc/ldap#g" ${DIR}/slapd.test.config.ldif
 sed -i "s#@LIB_DIR@#${DIR}/build/openldap/libexec/openldap#g" ${DIR}/slapd.test.config.ldif
 sed -i "s#@DB_DIR@#${DIR}/build/data#g" ${DIR}/slapd.test.config.ldif
 ${DIR}/build/openldap/sbin/slapadd.sh -v -F ${DIR}/build/slapd.d -n 0 -l ${DIR}/slapd.test.config.ldif
