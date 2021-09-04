d2f4593887fdb5522e60a692c330bc7fc946ed6d
Boris Rybalkin <ribalkin@gmail.com>
2021-09-05 00:00:08 +0100

    openldap: move modules to preload to fix upgrade of existing config

--- openldap/ldapadd.sh
+++ openldap/ldapadd.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/bin/bash -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 LIBS=$(echo ${DIR}/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib)
+MODULES_RID=$DIR/usr/lib/ldap
+PRELOAD=$MODULES_RID/back_mdb.so:$MODULES_RID/ppolicy.so
 ${DIR}/lib/*-linux*/ld-*.so --library-path $LIBS ${DIR}/usr/bin/ldapadd "$@"
