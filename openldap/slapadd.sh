0e54ec82b2cbf8e9f13d5e05dd7f6dd68dfb43fb
Boris Rybalkin <ribalkin@gmail.com>
2021-09-15 21:54:12 +0100

    openldap: revert preload, add ldapsearch.sh

--- openldap/slapadd.sh
+++ openldap/slapadd.sh
@@ -3,6 +3,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 LIBS=$(echo ${DIR}/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib)
-MODULES_DIR=$DIR/usr/lib/ldap
-PRELOAD=$MODULES_DIR/$(readlink $MODULES_DIR/back_mdb.so):$MODULES_DIR/$(readlink $MODULES_DIR/ppolicy.so)
-${DIR}/lib/*-linux*/ld-*.so --library-path $LIBS --preload $PRELOAD ${DIR}/usr/sbin/slapadd "$@"
+${DIR}/lib/*-linux*/ld-*.so --library-path $LIBS ${DIR}/usr/sbin/slapadd "$@"
