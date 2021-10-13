425116d3deec5f07d6574df0773f703601c8c557
Boris Rybalkin <ribalkin@gmail.com>
2021-10-13 20:00:49 +0100

    dovecot: fix test

--- dovecot/test.sh
+++ dovecot/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-${DIR}/build/dovecot/bin/dovecot.sh --version || true
+${DIR}/build/dovecot/bin/dovecot.sh --version
 ${DIR}/build/dovecot/bin/doveadm.sh --version || true
