baa8a3c3d37c0961c1dee3a96da1f391fed39188
Boris Rybalkin <ribalkin@gmail.com>
2021-10-14 07:28:55 +0100

    dovecot: fix auth bin

--- dovecot/test.sh
+++ dovecot/test.sh
@@ -4,4 +4,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ${DIR}/build/dovecot/bin/dovecot.sh --version
 ${DIR}/build/dovecot/bin/doveadm.sh --version || true
 ${DIR}/build/dovecot/libexec/dovecot/auth || true
-${DIR}/build/dovecot/libexec/dovecot/auth.sh || true
+${DIR}/build/dovecot/libexec/dovecot/auth.bin || true
