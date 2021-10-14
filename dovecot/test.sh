aa0992d6020174f00e6c3a632a5433e2a8854a16
Boris Rybalkin <ribalkin@gmail.com>
2021-10-14 21:50:04 +0100

    dovecot: fix test grep

--- dovecot/test.sh
+++ dovecot/test.sh
@@ -2,10 +2,10 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ${DIR}/build/dovecot/bin/dovecot.sh --version
-${DIR}/build/dovecot/bin/doveadm.sh --version | grep "doveadm(init)"
-${DIR}/build/dovecot/libexec/dovecot/auth.sh | grep "auth(init)"
+${DIR}/build/dovecot/bin/doveadm.sh 2>&1 | grep "doveadm(init)"
+${DIR}/build/dovecot/libexec/dovecot/auth.sh 2>&1 | grep "auth(init)"
 
 DOVECOT=${DIR}/build/dovecot
 export LD_LIBRARY_PATH=${DOVECOT}/lib:${DOVECOT}/lib/dovecot:${DOVECOT}/lib/dovecot/old-stats
 ldd ${DOVECOT}/libexec/dovecot/auth
-${DOVECOT}/libexec/dovecot/auth | grep "auth(init)"
+${DOVECOT}/libexec/dovecot/auth 2>&1 | grep "auth(init)"
