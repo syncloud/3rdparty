a07ec56dd48a7dda681e74c0ebe08becd064dd5c
Boris Rybalkin <ribalkin@gmail.com>
2021-10-17 22:54:03 +0100

    opendkim: try debian package

--- opendkim/build.sh
+++ opendkim/build.sh
@@ -10,40 +10,39 @@ export TMPDIR=/tmp
 export TMP=/tmp
 NAME=opendkim
 VERSION=2.10.3
+#VERSION=2-11-0-Beta2
 BUILD=${DIR}/build
 PREFIX=${BUILD}/${NAME}
 
 apt update
-apt-get -y install libbsd-dev libmilter-dev wget build-essential libssl-dev
+apt-get -y install opendkim opendkim-tools
 
 
-rm -rf ${BUILD}
-mkdir ${BUILD}
-cd ${BUILD}
+#export LD_LIBRARY_PATH=${PREFIX}/lib
+#cp --remove-destination /lib/*/libbsd.so* ${PREFIX}/lib
+#cp --remove-destination /usr/lib/*/libmilter*.so* ${PREFIX}/lib
+#cp --remove-destination /usr/lib/*/libcrypt*.so* ${PREFIX}/lib
+#cp --remove-destination /usr/lib/*/libssl*.so* ${PREFIX}/lib
 
-wget https://downloads.sourceforge.net/project/opendkim/opendkim-${VERSION}.tar.gz  --progress dot:giga -O ${NAME}-${VERSION}.tar.gz
-tar xzf ${NAME}-${VERSION}.tar.gz
-cd ${NAME}-${VERSION}
-./configure --prefix=${PREFIX} --disable-shared
-make
-make install
+mkdir -p $BUILD/$NAME
+mkdir -p $BUILD/$NAME/bin
+mkdir -p $BUILD/$NAME/sbin
 
-ls -la ${PREFIX}
-ls -la ${PREFIX}/lib
-ls -la ${PREFIX}/sbin
+cp /usr/sbin/opendkim $BUILD/$NAME/sbin
+cp /usr/bin/convert_keylist $BUILD/$NAME/bin
+cp /usr/bin/miltertest $BUILD/$NAME/bin
+cp /usr/bin/opendkim-atpszone $BUILD/$NAME/bin
+cp /usr/bin/opendkim-genkey $BUILD/$NAME/bin
+cp /usr/bin/opendkim-genzone $BUILD/$NAME/bin
+cp /usr/bin/opendkim-spam $BUILD/$NAME/bin
+cp /usr/bin/opendkim-stats $BUILD/$NAME/bin
+cp /usr/bin/opendkim-testkey $BUILD/$NAME/bin
+cp /usr/bin/opendkim-testmsg $BUILD/$NAME/bin
 
-export LD_LIBRARY_PATH=${PREFIX}/lib
-cp --remove-destination /lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libbsd.so* ${PREFIX}/lib
-cp --remove-destination /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libmilter*.so* ${PREFIX}/lib
-cp --remove-destination /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libcrypt*.so* ${PREFIX}/lib
-cp --remove-destination /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libssl*.so* ${PREFIX}/lib
+ldd /usr/sbin/opendkim
 
-ldd ${PREFIX}/sbin/opendkim
-
-${PREFIX}/sbin/opendkim --help || true
-${PREFIX}/sbin/opendkim-genkey --help || true
+/usr/sbin/opendkim --help || true
+/usr/bin/opendkim-genkey --help || true
 
 rm -rf ${BUILD}/${NAME}-${ARCH}.tar.gz
-tar czf ${DIR}/${NAME}-${ARCH}-${VERSION}.tar.gz -C ${BUILD} ${NAME}
-
 tar czf ${DIR}/${NAME}-${ARCH}-${VERSION}.tar.gz -C ${BUILD} ${NAME}
--- opendkim/test.sh
+++ opendkim/test.sh
@@ -7,32 +7,4 @@ export LD_LIBRARY_PATH=${DIR}/build/opendkim/lib
 ldd ${DIR}/build/opendkim/sbin/opendkim
 
 ${DIR}/build/opendkim/sbin/opendkim --help || true
-${DIR}/build/opendkim/sbin/opendkim-genkey --help || true
-ib/*/libcrypt*.so* ${PREFIX}/lib
-cp --remove-destination /usr/lib/*/libssl*.so* ${PREFIX}/lib
-
-ldd ${DIR}/build/opendkim/sbin/opendkim
-
-${PREFIX}/sbin/opendkim --help || true
-${PREFIX}/sbin/opendkim-genkey --help || true
-
-PREFIX}/sbin/opendkim
-
-${PREFIX}/sbin/opendkim --help || true
-${PREFIX}/sbin/opendkim-genkey --help || true
-
-H=${PREFIX}/lib
-cp --remove-destination /lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libbsd.so* ${PREFIX}/lib
-cp --remove-destination /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libmilter*.so* ${PREFIX}/lib
-cp --remove-destination /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libcrypt*.so* ${PREFIX}/lib
-cp --remove-destination /usr/lib/$(dpkg-architecture -q DEB_HOST_GNU_TYPE)/libssl*.so* ${PREFIX}/lib
-
-ldd ${PREFIX}/sbin/opendkim
-
-${PREFIX}/sbin/opendkim --help || true
-${PREFIX}/sbin/opendkim-genkey --help || true
-
-rm -rf ${BUILD}/${NAME}-${ARCH}.tar.gz
-tar czf ${DIR}/${NAME}-${ARCH}-${VERSION}.tar.gz -C ${BUILD} ${NAME}
-
-tar czf ${DIR}/${NAME}-${ARCH}-${VERSION}.tar.gz -C ${BUILD} ${NAME}
+${DIR}/build/opendkim/bin/opendkim-genkey --help || true
