0e136c14fd7f21d8d8193c1e02627c492e9f59eb
Boris Rybalkin <ribalkin@gmail.com>
2021-11-14 16:01:09 +0000

    postgresql-10: fix test

--- postgresql-10/bin/pg_dumpall.sh
+++ postgresql-10/bin/pg_dumpall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 LIBS=$(echo ${DIR}/lib)
-exec ${DIR}/lib/ld.so --library-path $LIBS ${DIR}/bin/pg_dumpall.bin "$@"
+exec ${DIR}/lib/ld.so --library-path $LIBS ${DIR}/bin/pg_dumpall "$@"
