1367e7caea9a20ef11c10ba79b274e2840fccbc1
Boris Rybalkin <ribalkin@gmail.com>
2022-06-13 10:38:15 +0100

    btrfs python-3.7-dev

--- btrfs/build.sh
+++ btrfs/build.sh
@@ -12,7 +12,7 @@ VERSION=5.18.1
 PREFIX=${DIR}/build/${NAME}
 
 apt update
-apt -y install build-essential wget pkg-config autoconf python3-sphinx e2fslibs-dev reiserfsprogs libblkid-dev liblzo2-dev zlib1g-dev libzstd-dev libudev-dev python3.7
+apt -y install build-essential wget pkg-config autoconf python3-sphinx e2fslibs-dev reiserfsprogs libblkid-dev liblzo2-dev zlib1g-dev libzstd-dev libudev-dev python3.7-dev
 
 rm -rf build
 mkdir -p build
