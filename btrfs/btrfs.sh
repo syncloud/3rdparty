34de25ed23929a9efcba7440bdf9b039ba94e125
Boris Rybalkin <ribalkin@gmail.com>
2022-06-13 22:09:21 +0100

    btrfs fix test

--- btrfs/btrfs.sh
+++ btrfs/btrfs.sh
@@ -4,4 +4,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
 export LD_LIBRARY_PATH=${DIR}/lib
 export PATH=${DIR}/bin:${PATH}
-exec ${DIR}/lib/ld.so ${DIR}/bin/btrfs
+exec ${DIR}/lib/ld.so ${DIR}/bin/btrfs "$@"
