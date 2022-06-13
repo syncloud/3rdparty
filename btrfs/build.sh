becac34f257ec5b1c8d8cd9f96d2c221edf1c39c
Boris Rybalkin <ribalkin@gmail.com>
2022-06-13 10:22:03 +0100

    btrfs libs

--- btrfs/build.sh
+++ btrfs/build.sh
@@ -12,7 +12,7 @@ VERSION=5.18.1
 PREFIX=${DIR}/build/${NAME}
 
 apt update
-apt -y install build-essential wget pkg-config autoconf python3-sphinx e2fslibs-dev reiserfsprogs libblkid-dev liblzo2-dev
+apt -y install build-essential wget pkg-config autoconf python3-sphinx e2fslibs-dev reiserfsprogs libblkid-dev liblzo2-dev zlib1g-dev
 
 rm -rf build
 mkdir -p build
