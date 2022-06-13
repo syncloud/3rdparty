515631f35a60e9f58a728886707a1b41b5f572e5
Boris Rybalkin <ribalkin@gmail.com>
2022-06-13 10:10:41 +0100

    btrfs fs progs

--- btrfs/build.sh
+++ btrfs/build.sh
@@ -12,7 +12,7 @@ VERSION=5.18.1
 PREFIX=${DIR}/build/${NAME}
 
 apt update
-apt -y install build-essential wget pkg-config autoconf python3-sphinx
+apt -y install build-essential wget pkg-config autoconf python3-sphinx e2fsprogs reiserfsprogs
 
 rm -rf build
 mkdir -p build
